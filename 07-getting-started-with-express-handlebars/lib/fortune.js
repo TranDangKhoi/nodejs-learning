586002ed949012e91c16396d201e9538843a3cc9
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-11-10 16:30:45 +0700

    :+1: Update fortune

--- 07-getting-started-with-express-handlebars/lib/fortune.js
+++ 07-getting-started-with-express-handlebars/lib/fortune.js
@@ -1,9 +1,9 @@
 const fortuneCookies = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
+  "Study now or you will regret later.",
+  "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.",
+  "It’s hard enough to find an error in your code when you’re looking for it; it’s even harder when you’ve assumed your code is error-free.",
+  "Good code is its own best documentation. As you’re about to add a comment, ask yourself, “How can I improve the code so that this comment isn’t needed?” Improve the code and then document it to make it even clearer.",
+  "One of my most productive days was throwing away 1000 lines of code.",
 ];
 
 exports.getFortune = () => {
