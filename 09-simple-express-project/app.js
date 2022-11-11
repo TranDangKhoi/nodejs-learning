89c5c28183eb29d89d5e10ac5ff96e8041dff7db
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-11-11 15:45:06 +0700

    :+1: Update code with my own UI and template

--- 09-simple-express-project/app.js
+++ 09-simple-express-project/app.js
@@ -4,12 +4,20 @@ const expbs = require("express-handlebars").engine;
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.engine("handlebars", expbs());
+app.engine(
+  "handlebars",
+  expbs({
+    defaultLayout: "main",
+  })
+);
 app.set("view engine", "handlebars");
-
+app.use(express.static(__dirname + "/public"));
 // Routing
 app.get("/", (req, res) => {
   res.render("home");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(port, () => {
