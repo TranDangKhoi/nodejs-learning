739c9f099f6553726df754f635aef99f100b4c6f
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-11-14 23:56:12 +0700

    :sunglasses: Update chapter 6

--- 11-chapter-6/app.js
+++ 11-chapter-6/app.js
@@ -38,9 +38,25 @@ app.get("/greeting", (req, res) => {
 });
 
 app.get("/no-layout", (req, res) => res.render("no-layout", { layout: null }));
+
 app.get("/custom-layout", (req, res) => {
   res.render("custom-layout", { layout: "custom" });
 });
+
+app.use((err, req, res, next) => {
+  console.error("** SERVER ERROR: " + err.message);
+  res
+    .status(500)
+    .render("08-error", { message: "you shouldn't have clicked that!" });
+});
+
+app.use((req, res) => res.status(404).render("404"));
+app.post("/process-contact", (req, res) => {
+  console.log(`received contact from ${req.body.name}
+<${req.body.email}>`);
+  res.redirect(303, "10-thank-you");
+});
+
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
 });
