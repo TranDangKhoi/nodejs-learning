c07820b13577f05860ab2920969d3a8f92c502be
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-11-14 21:35:33 +0700

    :thinking: Testing out greetings, still don't understand this one tho

--- 11-chapter-6/app.js
+++ 11-chapter-6/app.js
@@ -28,6 +28,15 @@ app.get("/errors", (req, res) => {
   res.status(500).render("errors");
 });
 
+app.get("/greeting", (req, res) => {
+  res.render("greeting", {
+    message: "Hello esteemed programmer!",
+    style: req.query.style,
+    userid: req.cookies.userid,
+    username: req.session.username,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
 });
