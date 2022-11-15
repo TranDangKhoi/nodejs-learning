402898e620ddb89e4c0b7184e36b6ae9947d621e
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-11-15 14:18:00 +0700

    :heart: Get form's input value and logged it out

--- 11-chapter-6/app.js
+++ 11-chapter-6/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const expbs = require("express-handlebars").engine;
 const port = 3000;
 const app = express();
-
+const bodyParser = require("body-parser");
+const e = require("express");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.engine(
   "handlebars",
   expbs({
@@ -32,8 +34,8 @@ app.get("/greeting", (req, res) => {
   res.render("greeting", {
     message: "Hello esteemed programmer!",
     style: req.query.style,
-    userid: req.query.userid,
-    username: req.query.username,
+    userid: req.cookies.userid,
+    username: req.session.username,
   });
 });
 
@@ -43,18 +45,27 @@ app.get("/custom-layout", (req, res) => {
   res.render("custom-layout", { layout: "custom" });
 });
 
+// Basic form processing
+app.get("/thank-you", (req, res) => {
+  res.render("thank-you", {});
+});
+app.get("/contact", (req, res) => {
+  res.render("contact", {});
+});
+
+app.post("/contact", (req, res) => {
+  console.log(`received contact from ${req.body.name}
+  <${req.body.email}>`);
+  res.redirect(303, "thank-you");
+});
+
+app.use((req, res) => res.status(404).render("404"));
+
 app.use((err, req, res, next) => {
   console.error("** SERVER ERROR: " + err.message);
   res
     .status(500)
-    .render("08-error", { message: "you shouldn't have clicked that!" });
-});
-
-app.use((req, res) => res.status(404).render("404"));
-app.post("/process-contact", (req, res) => {
-  console.log(`received contact from ${req.body.name}
-<${req.body.email}>`);
-  res.redirect(303, "10-thank-you");
+    .render("error", { message: "you shouldn't have clicked that!" });
 });
 
 app.listen(port, () => {
